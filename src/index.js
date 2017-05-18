8962295f864d84d98d82c148b10c1ce0464a8121
Jan Heuermann <jan@jotaen.net>
2017-05-18 11:35:15 +0200

    fix(hello-world) Yay, the first bug! 🐞

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send({ hello: world });
+    res.send({ hello: 'world' });
 });
 
 app.listen(8080, function() {
