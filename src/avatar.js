94ae6c9d0bc67657e2fdd85d59374652d4d51ca4
Jan Heuermann <jan@jotaen.net>
2017-05-18 14:51:17 +0200

    feat(avatar) Switch to Avenir 💄

--- src/avatar.js
+++ src/avatar.js
@@ -5,6 +5,22 @@ module.exports = (initials, color) =>
         xmlns="http://www.w3.org/2000/svg"
         version="1.1"
         viewBox="0 0 ${SIZE} ${SIZE}">
-            <rect x="0" y="0" width="${SIZE}" height="${SIZE}" fill="${color || '#ffeeee'}" stroke-width="0" />
-            <text x="50%" y="68%" text-anchor="middle" fill="white" font-family="Helvetica, Arial, sans-serif" font-size="${SIZE * 0.5}">${initials}</text>
+            <rect
+                x="0"
+                y="0"
+                width="${SIZE}"
+                height="${SIZE}"
+                fill="${color || '#ffeeee'}"
+                stroke-width="0"
+            />
+            <text
+                x="50%"
+                y="68%"
+                text-anchor="middle"
+                fill="white"
+                font-family="'Avenir Next', Helvetica, Arial, sans-serif"
+                font-weight="500"
+                font-size="${SIZE * 0.5}">
+                ${initials}
+            </text>
     </svg>`;
