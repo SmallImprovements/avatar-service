507ef286217f9f3ecf41eb9cb75d9563924bdf3a
Jan Heuermann <jan@jotaen.net>
2017-05-18 10:49:19 +0200

    chore(*) Setup npm project with express server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send({ hello: world });
+});
+
+app.listen(8080, function() {
+    console.log('App is running');
+});
