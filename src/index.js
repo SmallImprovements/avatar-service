b064200ea034e8e757830ba95c8cb8e311781ed8
Jan Heuermann <jan@jotaen.net>
2017-05-18 14:29:53 +0200

    chore(server) Activate logging

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ const app = express();
 const avatar = require('./avatar');
 const validate = require('./validate');
 const color = require('./color');
+const expressWinston = require('express-winston');
+const winston = require('winston');
 
 const port = process.env.PORT || 8080;
+
+const requestLogger = expressWinston.logger({
+    transports: [
+        new winston.transports.Console({
+            json: false,
+            colorize: false,
+        }),
+    ],
+    expressFormat: true,
+    meta: false,
+});
+
+app.use(requestLogger);
 
 app.get('/:index/:initials.svg', function(req, res) {
     const { initials, index } = req.params;
