5762264c84a8d48d2848cae31d8141dab948da11
k-lange <mail@klange.org>
2017-05-18 12:33:26 +0200

    feat(color) choose fill color by index

--- src/avatar.js
+++ src/avatar.js
@@ -1,10 +1,10 @@
 const SIZE = 400;
 
-module.exports = initials =>
+module.exports = (initials, color) =>
     `<svg
         xmlns="http://www.w3.org/2000/svg"
         version="1.1"
         viewBox="0 0 ${SIZE} ${SIZE}">
-            <rect x="0" y="0" width="${SIZE}" height="${SIZE}" fill="#ffeeee" stroke-width="0" />
+            <rect x="0" y="0" width="${SIZE}" height="${SIZE}" fill="${color || '#ffeeee'}" stroke-width="0" />
             <text x="50%" y="68%" text-anchor="middle" fill="white" font-family="Helvetica, Arial, sans-serif" font-size="${SIZE * 0.5}">${initials}</text>
     </svg>`;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 const avatar = require('./avatar');
 const validate = require('./validate');
+const color = require('./color');
 
 const port = process.env.PORT || 8080;
 
-app.get('/:initials', function(req, res) {
-    const initials = req.params.initials;
+app.get('/:index/:initials', function(req, res) {
+    const { initials, index } = req.params;
+    const avatarColor = color(index);
     if (!validate(initials)) {
         res.status(400).send({});
         return;
     }
-    res.type('image/svg+xml').send(avatar(initials));
+    res.type('image/svg+xml').send(avatar(initials, avatarColor));
 });
 
 app.listen(port, function() {
