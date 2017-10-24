46799c28948fd58cc5f9e850d7accd517bcc6d72
Jan Heuermann <jan@jotaen.net>
2017-10-24 10:27:39 +0200

    Add base URL to tests

--- src/app.test.js
+++ src/app.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 const app = require('./app');
 const assert = require('assert');
 
+const BASE_URL = '/api/avatars';
+
 binaryParser = (res, callback) => {
     res.setEncoding('binary');
     res.data = '';
@@ -16,25 +18,25 @@ binaryParser = (res, callback) => {
 describe('Avatar service', () => {
     describe('SVG rendering', () => {
         it('should output the correct content type', done => {
-            request(app).get('/1/JH.svg').expect(200).expect('Content-Type', /image\/svg\+xml/).end(done);
+            request(app).get(BASE_URL + '/1/JH.svg').expect(200).expect('Content-Type', /image\/svg\+xml/).end(done);
         });
         it('should handle HTML opening braces correctly', done => {
-            request(app).get('/5/<.svg').expect(200).expect('Content-Type', /image\/svg\+xml/).end(done);
+            request(app).get(BASE_URL + '/5/<.svg').expect(200).expect('Content-Type', /image\/svg\+xml/).end(done);
         });
         it('should render an SVG element', done => {
-            request(app).get('/5/KL.svg').expect(200).parse(binaryParser).end((err, res) => {
+            request(app).get(BASE_URL + '/5/KL.svg').expect(200).parse(binaryParser).end((err, res) => {
                 if (/<svg(.|\n)*?<\/svg>/.test(res.body.toString())) done();
                 else done(new Error());
             });
         });
         it('should render a path element', done => {
-            request(app).get('/12/PF.svg').expect(200).parse(binaryParser).end((err, res) => {
+            request(app).get(BASE_URL + '/12/PF.svg').expect(200).parse(binaryParser).end((err, res) => {
                 if (/<path(.|\n)*?\/>/.test(res.body.toString())) done();
                 else done(new Error());
             });
         });
         it('should always render a default color, even if `:index` is out of bounds', done => {
-            request(app).get('/182736178123761/AW.svg').expect(200).parse(binaryParser).end((err, res) => {
+            request(app).get(BASE_URL + '/182736178123761/AW.svg').expect(200).parse(binaryParser).end((err, res) => {
                 if (/#ffeeee/.test(res.body.toString())) done();
                 else done(new Error());
             });
@@ -42,7 +44,7 @@ describe('Avatar service', () => {
     });
     describe('Request validation', () => {
         it('should return an error if initials are longer then 2 chars', done => {
-            request(app).get('/15/ABC.svg').expect(400).end(done);
+            request(app).get(BASE_URL + '/15/ABC.svg').expect(400).end(done);
         });
     });
 });
