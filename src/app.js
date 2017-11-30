2708302542f450d668c6de9390902bc677562612
Matthew Reid <mreid@small-improvements.com>
2017-11-30 11:57:11 +0100

    feat(app) Support png avatars by converting the svg

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ const compression = require('compression');
 const path = require('path');
 const textToSVG = require('text-to-svg').loadSync(path.resolve(__dirname, 'font.woff'));
 const COLOR = require('./color');
+const svg2img = require('svg2img');
 const app = express();
 app.use(compression());
 
 const SIZE = 400;
 const FONT_SIZE = SIZE * 0.5;
+
+const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args), ...args));
 
 const svg = (initials, color) => {
     const path = textToSVG.getPath(initials, {
@@ -20,7 +23,7 @@ const svg = (initials, color) => {
     return `<svg
         xmlns="http://www.w3.org/2000/svg"
         version="1.1"
-        viewBox="0 0 ${SIZE} ${SIZE}">
+        viewBox="0 0 ${SIZE} ${SIZE}" width="${SIZE}" height="${SIZE}">
             <rect
                 x="0"
                 y="0"
@@ -33,25 +36,52 @@ const svg = (initials, color) => {
     </svg>`;
 };
 
-module.exports = app.get(
-    '/api/avatars/:index/:initials.svg',
-    (req, res, next) => {
-        const isValid = (() => {
-            const { initials } = req.params;
-            if (typeof initials !== 'string') return false;
-            if (initials.length < 1 || initials.length > 2) return false;
-            return true;
-        })();
-        if (!isValid) {
-            res.status(400).send('Invalid requests parameters');
-            return;
-        }
-        next();
+const toSvg = (req, res) => {
+    const { initials, index } = req.params;
+    const color = COLOR[index];
+    return svg(initials, color);
+};
+
+const handleSvg = compose((avatar, req, res) => {
+    res.type('image/svg+xml').send(avatar);
+}, toSvg);
+
+const handlePng = compose(
+    (avatarPromise, svg, req, res) => {
+        avatarPromise
+            .then(avatar => res.type('image/png').send(avatar))
+            .catch(err => res.status(500).send('Internal error'));
     },
-    (req, res) => {
-        const { initials, index } = req.params;
-        const color = COLOR[index];
-        const avatar = svg(initials, color);
-        res.type('image/svg+xml').send(avatar);
-    }
+    avatarSvg => {
+        return new Promise((resolve, reject) => {
+            svg2img(avatarSvg, (error, avatar) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(avatar);
+                }
+            });
+        });
+    },
+    toSvg
 );
+
+const isValid = (req, res, next) => {
+    const isValid = (() => {
+        const { initials } = req.params;
+        if (typeof initials !== 'string') return false;
+        if (initials.length < 1 || initials.length > 2) return false;
+        return true;
+    })();
+    if (!isValid) {
+        res.status(400).send('Invalid requests parameters');
+        return;
+    }
+    next();
+};
+
+app.get('/api/avatars/:index/:initials.svg', isValid, handleSvg);
+
+app.get('/api/avatars/:index/:initials.png', isValid, handlePng);
+
+module.exports = app;
